40de5984949355d83cbb6fbc105a21034832dde4
Rui Lima <r7lima3@hotmail.com>
2023-03-14 16:33:48 +0000

    Parte do Essentials Real-Time 3D.4

---
+++ Assets/Scripts/HitGroundEffect2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitGroundEffect2 : MonoBehaviour
+{
+    public AudioClip collisionSound;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "plane")
+        {
+            audioSource.PlayOneShot(collisionSound);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
